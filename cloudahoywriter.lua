7035809d4330ecb505fbb8da0813e20ff42f878e
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-17 18:45:40 -0700

    Create empty Lua file

---
+++ cloudahoywriter.lua
@@ -0,0 +1,6 @@
+-----------------------------------------------
+-- CloudAhoy Writer
+--  Original Python by Adrian Velicu
+--  Lua port by Phil Verghese
+-----------------------------------------------
+local versionNum = "0.0.1"
