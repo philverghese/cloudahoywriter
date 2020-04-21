56c8fdb8ed7888f121f0d195b0bd55ff284f0c1b
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-21 09:12:55 -0700

    Gray box shows on screen
    Using graphics module.

--- cloudahoywriter.lua
+++ cloudahoywriter.lua
@@ -4,3 +4,24 @@
 --  Lua port by Phil Verghese
 -----------------------------------------------
 local versionNum = "0.0.1"
+
+require('graphics')
+
+-- Bounds for control box
+local x1 = 0
+local x2 = 80
+local y1 = (SCREEN_HIGHT / 2) - 200
+local y2 = y1 + 100
+local centerX = (x2 - x1) / 2
+local centerY = (y2 - y1) / 2
+
+-- colors
+local background = {r=0.2, g=0.2, b=0.2, a=0.75}
+
+function CAWR_show_ui()
+    XPLMSetGraphicsState(0, 0, 0, 1, 1, 0, 0)
+    graphics.set_color(background.r, background.g, background.b, background.a)
+    graphics.draw_rectangle(x1, y1, x2, y2)
+end
+
+do_every_draw("CAWR_show_ui()")
