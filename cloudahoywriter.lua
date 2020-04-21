a8d617e142f3f0db94a6a4d7abba22a77d657eff
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-21 13:37:05 -0700

    Add CAWR text

--- cloudahoywriter.lua
+++ cloudahoywriter.lua
@@ -1,27 +1,40 @@
 -----------------------------------------------
 -- CloudAhoy Writer
 --  Original Python by Adrian Velicu
---  Lua port by Phil Verghese
+--  Lua version by Phil Verghese
 -----------------------------------------------
-local versionNum = "0.0.1"
+local versionNum = '0.0.1'
 
 require('graphics')
 
 -- Bounds for control box
 local x1 = 0
 local x2 = 80
-local y1 = (SCREEN_HIGHT / 2) - 200
+local y1 = (SCREEN_HIGHT / 2) - 50
 local y2 = y1 + 100
 local centerX = (x2 - x1) / 2
 local centerY = (y2 - y1) / 2
 
--- colors
-local background = {r=0.2, g=0.2, b=0.2, a=0.75}
+--------------------- colors
+-- background
+local bgR = 0.2
+local bgG = 0.2
+local bgB = 0.2
+local bgA = 0.8
+
+-- foreground
+local fgR = 0.8
+local fgG = 0.8
+local fgB = 0.8
+local fgA = 0.8
+
 
 function CAWR_show_ui()
     XPLMSetGraphicsState(0, 0, 0, 1, 1, 0, 0)
-    graphics.set_color(background.r, background.g, background.b, background.a)
+    graphics.set_color(bgR, bgG, bgB, bgA)
     graphics.draw_rectangle(x1, y1, x2, y2)
+    graphics.set_color(fgR, fgG, fgB, fgA)
+    draw_string(x1 + 5, y2 - 20, "CAWR")
 end
 
 do_every_draw("CAWR_show_ui()")
