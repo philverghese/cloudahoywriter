201a92c55a788806636f35ab22d81a5a8601ecb5
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-21 16:49:13 -0700

    Record start time. Display not calculated.

--- cloudahoywriter.lua
+++ cloudahoywriter.lua
@@ -8,11 +8,14 @@ local versionNum = '0.0.1'
 require('graphics')
 
 -- State
-local is_recording = false
 local enable_auto_hide = true
+local is_recording = false
+local flight_number = LUA_RUN
+local recording_start_time = nil
+local recording_display_time = "00:00:00"
 
 -- Bounds for control box
-local width = measure_string("X99:99X")
+local width = measure_string("X99:99:99X")
 local height = 90
 local cawr_width = measure_string("CAWR")
 local x1 = 0
@@ -20,32 +23,45 @@ local x2 = x1 + width
 local y1 = (SCREEN_HIGHT / 2) - 50
 local y2 = y1 + height
 local centerX = x1 + (width / 2)
-local centerY = y1 + ((y2 -y1) / 2)
+local centerY = y1 + ((y2 - y1) / 2)
 
----------- colors
--- background
+-- background color
 local bgR = 0.2
 local bgG = 0.2
 local bgB = 0.2
 local bgA = 0.8
 
--- foreground
+-- foreground color
 local fgR = 0.8
 local fgG = 0.8
 local fgB = 0.8
 local fgA = 0.8
 
--- recording off
+-- recording off color
 local recOffR = 0.05
 local recOffG = 0.05
 local recOffB = 0.05
 local recOffA = 0.8
 
--- recording on
+-- recording on color
 local recOnR = 0.9
 local recOnG = 0.2
 local recOnB = 0.2
 local recOnA = 0.8
+
+local function toggle_recording_state()
+    if recording_start_time == nil then
+        recording_start_time = os.clock()
+    else
+        recording_start_time = nil
+    end
+end
+
+local function get_recording_display_time()
+    if recording_display_time == nil then return "00:00:00" end
+    local current_time = os.clock()
+    return "00:02:04"
+end
 
 function CAWR_show_ui()
     if enable_auto_hide and (MOUSE_X > width * 3) then
@@ -60,7 +76,7 @@ function CAWR_show_ui()
 
     -- Foreground lines and text
     graphics.set_color(fgR, fgG, fgB, fgA)
-    draw_string(centerX - (cawr_width / 2), y2 - 20, "CAWR")
+    draw_string(centerX - (cawr_width / 2), y2 - 16, "CAWR")
     graphics.set_width(2)
     graphics.draw_line(x1, y2, x2, y2)
     graphics.draw_line(x1, y2 - 30, x2, y2 - 30)
@@ -69,8 +85,9 @@ function CAWR_show_ui()
     graphics.draw_line(x1, y1, x2, y1)
 
     -- Recording circle
-    if (is_recording) then
+    if (recording_start_time ~= nil) then
         graphics.set_color(recOnR, recOnG, recOnB, recOnA)
+        recording_display_time = get_recording_display_time()
     else
         graphics.set_color(recOffR, recOffG, recOffB, recOffA)
     end
@@ -79,14 +96,15 @@ function CAWR_show_ui()
     graphics.draw_circle(centerX, centerY - 5, 12, 2)
 
     -- Recording time
-    draw_string(centerX - (measure_string("12:34") / 2), y1 + 10, "12:34")
+    draw_string(centerX - (measure_string(recording_display_time) / 2),
+        y1 + 10, recording_display_time)
 end
 
 function CAWR_on_mouse_click()
     if MOUSE_X < x1 or MOUSE_X > x2 then return end
     if MOUSE_Y < y1 or MOUSE_Y > y2 then return end
     if MOUSE_STATUS == 'up' then
-        is_recording = not is_recording
+        toggle_recording_state()
     end
 
     RESUME_MOUSE_CLICK = true -- consume click
