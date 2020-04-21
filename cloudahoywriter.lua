0863c9ea9029d6e97df3474059b7ec5a688a5a94
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-21 13:56:23 -0700

    Center text and draw white borders

--- cloudahoywriter.lua
+++ cloudahoywriter.lua
@@ -8,8 +8,10 @@ local versionNum = '0.0.1'
 require('graphics')
 
 -- Bounds for control box
+local width = measure_string("XX99:99XX")
+local cawr_width = measure_string("CAWR")
 local x1 = 0
-local x2 = 80
+local x2 = x1 + width
 local y1 = (SCREEN_HIGHT / 2) - 50
 local y2 = y1 + 100
 local centerX = (x2 - x1) / 2
@@ -28,13 +30,19 @@ local fgG = 0.8
 local fgB = 0.8
 local fgA = 0.8
 
-
 function CAWR_show_ui()
     XPLMSetGraphicsState(0, 0, 0, 1, 1, 0, 0)
+
     graphics.set_color(bgR, bgG, bgB, bgA)
     graphics.draw_rectangle(x1, y1, x2, y2)
+
     graphics.set_color(fgR, fgG, fgB, fgA)
-    draw_string(x1 + 5, y2 - 20, "CAWR")
+    draw_string(x1 + (width / 2) - (cawr_width / 2), y2 - 20, "CAWR")
+    graphics.draw_line(x1, y2, x2, y2)
+    graphics.draw_line(x1, y2 - 30, x2, y2 - 30)
+    graphics.draw_line(x1 + 1, y2, x1, y1)
+    graphics.draw_line(x2 + 1, y2, x2, y1)
+    graphics.draw_line(x1, y1, x2, y1)
 end
 
 do_every_draw("CAWR_show_ui()")
